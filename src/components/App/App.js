61ec83e1464aa28673a45cc8d426ee752522cb79
Daniel Wiesner <daniel_wiesner@gmx.de>
2021-02-22 15:34:02 +0100

    Add basic button component. Styling to be done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,22 @@
 import './App.css';
 import PlayerForm from '../PlayerForm/PlayerForm';
 import Player from '../Player/Player';
+import Button from '../Button/Button';
 
 function App() {
   return (
     <div className="App">
       <PlayerForm onAddPlayer={event => onAddPlayer(event)}/>
-      <Player name={"John Doe"} score={0} onMinus={() => console.log("Minus was clicked")} onPlus={() => console.log("Plus was clicked")}/>
+      <Player name={"John Doe"} score={20} onMinus={() => console.log("Minus was clicked")} onPlus={() => console.log("Plus was clicked")}/>
+      <Player name={"Jane Doe"} score={30} onMinus={() => console.log("Minus was clicked")} onPlus={() => console.log("Plus was clicked")}/>
+      <Button text="Reset scores" onClick={() => console.log("Scores were reset")} />
+      <Button text="Reset all" onClick={() => console.log("Everything was reset")} />
     </div>
   );
 }
 
 export default App;
+
 
 function onAddPlayer(event) {
   event.preventDefault()
